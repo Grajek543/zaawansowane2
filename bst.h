4ad44b10c1c3ab8dabd5e8b556f3bb5c3349623d
Szymon Biel <bszymon31@gmail.com>
2024-11-05 23:03:39 +0100

    Deklaracja drzewa BST

--- bst.h
+++ bst.h
@@ -3,5 +3,19 @@ struct wezel {
   wezel *lewy;
   wezel *prawy;
   int war;
-}
+};
 
+class bst {
+public:
+    int roz;
+    wezel *korzen;
+    bst();
+    ~bst() {}
+    void dodele(bst *&korzen, int a);
+    wezel *usuele(bst *&korzen, wezel a);
+    wezel *szuk(bst *&korzen, int a);
+    void wyspre(bst *&korzen);
+    void wysin(bst *&korzen);
+    void wyspost(bst *&korzen);
+    void zapis(bst *&korzen);
+}
