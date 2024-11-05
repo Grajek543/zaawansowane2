4f6405de78784f27c91a6f6bbd7c61e0e385347f
Szymon Biel <bszymon31@gmail.com>
2024-11-05 21:16:22 +0100

    Fundamenty programu

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "bst.h"
+#include "wczyt.h"
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+    std::cout << "Hello World";
+}
---
+++ wczyt.cpp
@@ -0,0 +1,2 @@
+#include "wczyt.h"
+#include <iostream>
