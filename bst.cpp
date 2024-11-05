78ca178fa7b41593449bb5bd847134534e9f43de
Szymon Biel <bszymon31@gmail.com>
2024-11-05 23:27:03 +0100

    Definiowanie metod drzewa cz. 1

--- bst.cpp
+++ bst.cpp
@@ -1,2 +1,34 @@
 #include "bst.h"
 #include <iostream>
+
+bst::bst(void) {
+    roz = 0;
+    korzen = NULL;
+}
+
+void dodele(wezel *&korzen, int a) {
+    wezel *b = new wezel;
+    wezel *c = korzen;
+    b->lewy = NULL;
+    b->prawy = NULL;
+    b->war = a;
+    if(!c) korzen = b;
+    else
+        while(true)
+            if(b->war < c->war) {
+                if(!c->lewy) {
+                    c->lewy = b;
+                    break;
+                    }
+                else c = c->lewy;
+            }
+            else {
+                if(!c->prawy) {
+                c->prawy = b;
+                break;
+                }
+                else c = c->prawy;
+            }
+    b->pop = c;
+}
+
