19e0a6205c26f63a0502745dc523ff6940a7b8ca
Szymon Biel <bszymon31@gmail.com>
2024-11-06 16:04:34 +0100

    Definiowanie metod drzewa cz. 2

--- bst.cpp
+++ bst.cpp
@@ -6,7 +6,39 @@ bst::bst(void) {
     korzen = NULL;
 }
 
-void dodele(wezel *&korzen, int a) {
+wezel *bst::najmin(wezel *a) {
+    wezel *b = a;
+    while(b->lewy) b = b->lewy;
+    return b;
+}
+
+wezel *bst::najmax(wezel *a) {
+    wezel *b = a;
+    while(b->prawy) b = b->prawy;
+    return b;
+}
+
+wezel *bst::min(wezel *a) {
+    if(a->lewy) return najmax(a->lewy);
+    wezel *b;
+    do {
+        b = a;
+        a = a->pop;
+    } while(a && (a->prawy != b));
+    return a;
+}
+
+wezel *bst::max(wezel *a) {
+    if(a->prawy) return najmin(a->prawy);
+    wezel *b;
+    do {
+        b = a;
+        a = a->pop;
+    } while(a && (a->lewy != b));
+    return a;
+}
+
+void bst::dodele(wezel *&korzen, int a) {
     wezel *b = new wezel;
     wezel *c = korzen;
     b->lewy = NULL;
@@ -19,16 +51,47 @@ void dodele(wezel *&korzen, int a) {
                 if(!c->lewy) {
                     c->lewy = b;
                     break;
-                    }
+                }
                 else c = c->lewy;
             }
             else {
                 if(!c->prawy) {
-                c->prawy = b;
-                break;
+                    c->prawy = b;
+                    break;
                 }
                 else c = c->prawy;
             }
     b->pop = c;
 }
 
+wezel *bst::usunele(wezel *&korzen, wezel *a) {
+    wezel *b = a->pop;
+    wezel *c;
+    if((a->lewy) && (a->prawy)) {
+        if(rand() % 2) c = usunele(korzen, min(a));
+        else usunele(korzen, max(a));
+        c->lewy = a->lewy;
+        if(c->lewy) c->lewy->pop = c;
+        c->prawy = a->prawy;
+        if(c->prawy) c->prawy->pop = c;
+    }
+    else {
+        if(a->lewy) c = a->lewy;
+        else c = a->prawy;
+    }
+    if(c) c->pop = b;
+    if(!b) korzen = c;
+    else {
+        if(b->lewy == a) b->lewy = c;
+        else b->prawy = c;
+    }
+    return a;
+}
+
+void bst::usubst(wezel *a) {
+    if(a) {
+        usubst(a->lewy);
+        usubst(a->prawy);
+        delete a;
+    }
+}
--- bst.h
+++ bst.h
@@ -1,8 +1,8 @@
 struct wezel {
-  wezel *pop;
-  wezel *lewy;
-  wezel *prawy;
-  int war;
+    wezel *pop;
+    wezel *lewy;
+    wezel *prawy;
+    int war;
 };
 
 class bst {
@@ -11,11 +11,16 @@ public:
     wezel *korzen;
     bst();
     ~bst() {}
-    void dodele(bst *&korzen, int a);
-    wezel *usuele(bst *&korzen, wezel a);
-    wezel *szuk(bst *&korzen, int a);
-    void wyspre(bst *&korzen);
-    void wysin(bst *&korzen);
-    void wyspost(bst *&korzen);
-    void zapis(bst *&korzen);
-}
+    wezel *najmin(wezel *a);
+    wezel *najmax(wezel *a);
+    wezel *min(wezel *a);
+    wezel *max(wezel *a);
+    void dodele(wezel *&korzen, int a);
+    wezel *usunele(wezel *&korzen, wezel *a);
+    void usubst(wezel *a);
+    wezel *szuk(wezel *&korzen, int a);
+    void wyspre(wezel *&korzen);
+    void wysin(wezel *&korzen);
+    void wyspost(wezel *&korzen);
+    void zapis(wezel *&korzen);
+};
